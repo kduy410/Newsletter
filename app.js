4b42906159b7013e936f26b1e2b76d366a20d7c6
Khánh Duy <kduy410@gmail.com>
2023-04-12 02:15:30 +0700

    create success, failure route

--- app.js
+++ app.js
@@ -31,11 +31,18 @@ app.post("/", (req, res) => {
     const MAIL_CHIMP_API_KEY = 'd3493b2c2e3a9b99cb3cb2e6c3e17b01-us8'; // usX = 8
     const API_SERVER = 'us8'; // usX = 8
     const url = `https://${API_SERVER}.api.mailchimp.com/3.0/lists/${audienceID}`;
+    console.log(url);
     const options = {
         method: "POST",
         auth: `key:${MAIL_CHIMP_API_KEY}`,
     };
     const request = https.request(url, options, (r) => {
+        if (r.statusCode === 200) {
+            res.sendFile(path.join(__dirname, "success.html"));
+        } else {
+            console.log(r.statusCode);
+            res.sendFile(path.join(__dirname, "failure.html"));
+        }
         r.on('data', (d) => {
             var data = JSON.parse(d);
             console.log(data.new_members);
